de5f2670e8730dac8d028ea89ecd1014b22d4d2e
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2023-03-31 11:24:31 +0200

    fix: indenting

--- src/transformOpacity.ts
+++ src/transformOpacity.ts
@@ -6,13 +6,12 @@
 export function transformOpacity(
     value: string | number | undefined,
   ): string | number | undefined {
-    if (value === undefined) {
-      return value;
-    }
-    if (`${value}`.endsWith('%')) {
-      const percentValue = `${value}`.slice(0, -1);
-      return parseFloat(percentValue) / 100;
-    }
+  if (value === undefined) {
     return value;
   }
-  
+  if (`${value}`.endsWith('%')) {
+    const percentValue = `${value}`.slice(0, -1);
+    return parseFloat(percentValue) / 100;
+  }
+  return value;
+}
